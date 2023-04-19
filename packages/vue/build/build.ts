22953c8608298be42e766703864514ea22b65044
王伟 <wangw@vastio.com>
2023-04-19 22:50:10 +0800

    打包esm和cjs依赖

--- packages/vue/build/build.ts
+++ packages/vue/build/build.ts
@@ -2,15 +2,16 @@ import { build } from "esbuild";
 import { resolve } from "node:path";
 import { emptyDir } from "fs-extra";
 import vue from "unplugin-vue/esbuild";
+import type { BuildOptions, Format } from "esbuild";
 
 import { printInfo } from "./utils";
 import { pathSrc, pathOutput } from "./paths";
 
-function buildBundle() {
-  return build({
+function getBuildOptions(format: Format) {
+  const options: BuildOptions = {
     entryPoints: [resolve(pathSrc, "index.ts")],
-    target: 'es2018', 
-    platform: 'neutral',
+    target: "es2018",
+    platform: "neutral",
     plugins: [
       vue({
         isProduction: true,
@@ -18,9 +19,32 @@ function buildBundle() {
       }),
     ],
     bundle: true,
+    format,
+    minifySyntax: true,
     outdir: pathOutput,
-    external: ["vue"]
-  });
+    external: ["vue"],
+  };
+  return options;
+}
+
+function buildBundle() {
+  const doBuild = async (minify: boolean) => {
+    await Promise.all([
+      build({
+        ...getBuildOptions("esm"),
+        entryNames: `[name]${minify ? '.min' : ''}`,
+        minify,
+      }),
+      build({
+        ...getBuildOptions("cjs"),
+        entryNames: `[name]${minify ? '.min' : ''}`,
+        outExtension: { '.js': '.cjs' },
+        minify,
+      }),
+    ]);
+  };
+
+  return Promise.all([doBuild(false), doBuild(true)]);
 }
 
 printInfo("cleaning dist.");
